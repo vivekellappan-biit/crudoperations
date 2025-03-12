10c9c107012e4bc32357702ace05f07b11b6464a
Vivek Ellappan <vivekebiit@gmail.com>
2025-03-12 11:15:37 +0530

    initial commit

--- src/main/java/com/vivek/crudoperations/controller/UserController.java
+++ src/main/java/com/vivek/crudoperations/controller/UserController.java
@@ -2,31 +2,32 @@ package com.vivek.crudoperations.controller;
 
 import com.vivek.crudoperations.model.User;
 import com.vivek.crudoperations.service.UserService;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/users")
-@AllArgsConstructor
-
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
-
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return new ResponseEntity<User>(userService.insertUser(user), HttpStatus.CREATED);
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> allUsers = userService.getAllUsers();
-        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    @PostMapping
+    public User insertUser(@RequestBody User user) {
+        return  userService.insertUser(user);
+    }
+
+    @DeleteMapping
+    public void deleteUserById(Long userId) {
+        userService.deleteUserById(userId);
     }
 
 }
--- src/main/java/com/vivek/crudoperations/service/UserService.java
+++ src/main/java/com/vivek/crudoperations/service/UserService.java
@@ -1,13 +1,27 @@
 package com.vivek.crudoperations.service;
 
 import com.vivek.crudoperations.model.User;
+import com.vivek.crudoperations.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface UserService {
-    User insertUser(User user);
-    User findById(Long userId);
-    List<User> getAllUsers();
-    User updateUser(User user);
-    void  deleteUser(Long id);
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User insertUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public  void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
